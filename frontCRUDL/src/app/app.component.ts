5d4a152c736f597450738bab386406f28700068d
Héctor Go. An <hectorgoan@usal.es>
2019-08-10 11:37:41 +0200

    Moved the coded part to the home component

--- frontCRUDL/src/app/app.component.ts
+++ frontCRUDL/src/app/app.component.ts
@@ -1,7 +1,5 @@
 import { Component } from '@angular/core';
-import { ClientService } from './shared/services/client.service';
-import { Client } from './shared/models/client.model';
-import {map} from 'rxjs/operators';
+
 
 @Component({
   selector: 'app-root',
@@ -9,16 +7,5 @@ import {map} from 'rxjs/operators';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'frontCRUDL';
-  clients = null;
-
-  constructor(private clientService: ClientService) {}
-
-  getClientsList() {
-    this.clientService.getClients()
-      .pipe(map((data: Client) => this.clients = {data}))
-      .toPromise()
-      .then(x => console.log(this.clients));
-
-  }
+  constructor() {}
 }
--- frontCRUDL/src/app/home/home.component.ts
+++ frontCRUDL/src/app/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ClientService } from '../shared/services/client.service';
+import { Client } from '../shared/models/client.model';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
@@ -7,7 +10,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private clientService: ClientService) {}
+
+  title = 'frontCRUDL';
+  clients = null;
+
+  getClientsList() {
+    this.clientService.getClients()
+      .pipe(map((data: Client) => this.clients = {data}))
+      .toPromise()
+      .then(x => console.log(this.clients));
+
+  }
 
   ngOnInit() {
   }
